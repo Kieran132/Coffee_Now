e32f41e03bec900a26af1d6e0f89381394d78766
Kieran132 <kiemclean@hotmail.co.uk>
2023-03-31 12:15:02 +0000

    Add coffee/roasting/flavour models to order app

--- order/models.py
+++ order/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class CoffeeBean(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='coffee_beans/')
+    roasting_level = models.ForeignKey(
+        'RoastingLevel', on_delete=models.PROTECT)
+    flavor_profile = models.ForeignKey(
+        'FlavorProfile', on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class RoastingLevel(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class FlavorProfile(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
