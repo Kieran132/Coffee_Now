8f84b7c48cbafa27c1b8b1098e6a3a37057d8183
Kieran132 <kiemclean@hotmail.co.uk>
2023-03-31 12:29:32 +0000

    Add product html  page and detail

--- order/views.py
+++ order/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import CoffeeBean
 
-# Create your views here.
+
+def coffee_list(request):
+    coffee_beans = CoffeeBean.objects.all()
+    return render(request, 'coffee_list.html', {'coffee_beans': coffee_beans})
+
+
+def coffee_detail(request, coffee_id):
+    coffee = get_object_or_404(CoffeeBean, pk=coffee_id)
+    return render(request, 'coffee_detail.html', {'coffee': coffee})
