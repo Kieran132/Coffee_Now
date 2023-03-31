4c34c57f19498222ba37da506a2b9da0be35daff
Kieran132 <kiemclean@hotmail.co.uk>
2023-03-31 12:39:34 +0000

    Add models to admin.py

--- order/admin.py
+++ order/admin.py
@@ -1,1 +1,6 @@
 from django.contrib import admin
+from .models import CoffeeBean, RoastingLevel, FlavorProfile
+
+admin.site.register(CoffeeBean)
+admin.site.register(RoastingLevel)
+admin.site.register(FlavorProfile)
