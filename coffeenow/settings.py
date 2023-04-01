2b8ca3bdd586fe66443b55bc458752c7c8ef8bd9
Kieran132 <kiemclean@hotmail.co.uk>
2023-04-01 14:38:34 +0000

    Fix - reset sqldatabase and made new migrations

--- coffeenow/settings.py
+++ coffeenow/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
@@ -51,6 +55,12 @@ INSTALLED_APPS = [
     'accessories',
     'contact',
 ]
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- coffeenow/urls.py
+++ coffeenow/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('', include('order.urls')),
     path('', include('accessories.urls')),
