8e1488b7b431ffa3749d2fdab674722dca6f1cf9
Kieran132 <kiemclean@hotmail.co.uk>
2023-04-19 11:14:05 +0000

    Removed unwanted code from order.forms.py file

--- order/forms.py
+++ order/forms.py
@@ -7,9 +7,3 @@ class BeanForm(forms.ModelForm):
     class Meta:
         model = CoffeeBean
         fields = ['name', 'description', 'price', 'image', 'roasting_level', 'flavor_profile', 'strength_level']
-
-        #image = forms.ImageField(
-        #    label='Image', required=False)
-
-    #def __init__(self, *args, **kwargs):
-    #super().__init__(*args, **kwargs)
