75bfa57edce466c2ad06c71fe5e6c26ed65b2b18
Kieran132 <kiemclean@hotmail.co.uk>
2023-05-03 18:11:44 +0000

    Change static_root path

--- coffeenow/settings.py
+++ coffeenow/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR), 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
