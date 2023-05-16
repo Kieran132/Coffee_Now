bdc13ad10a1533c3e5d99a6a537812c66ced7caa
Kieran132 <>
2023-05-16 08:10:00 +0000

    Add if statement to coffee accessories if user does not add an image

--- coffeenow/urls.py
+++ coffeenow/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,4 @@ urlpatterns = [
     path('', include('contact.urls')),
     path('', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
